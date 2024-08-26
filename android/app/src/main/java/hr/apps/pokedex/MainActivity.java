af57b301323b48c285fa33ea9a85dd3e54f0e807
Jaime HR <jaimehr2101@gmail.com>
2024-08-26 13:35:45 -0600

    fix: add icon app

---
+++ android/app/src/main/java/hr/apps/pokedex/MainActivity.java
@@ -0,0 +1,5 @@
+package hr.apps.pokedex;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'pokedex',
+  appId: 'hr.apps.pokedex',
+  appName: 'Pokedex',
   webDir: 'www',
   bundledWebRuntime: false
 };
